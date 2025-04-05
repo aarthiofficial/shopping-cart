23ab9d0bad02ef96c73147e16dc2486c7d155572
aarthiofficial <aarthi.nbs@gmail.com>
2025-04-05 08:38:25 +0530

    Create ProductList.js

---
+++ src/components/ProductList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ProductList = ({ products, addToCart }) => {
+  return (
+    <div className="products">
+      <h2>Products</h2>
+      <div className="product-list">
+        {products.map((product) => (
+          <div key={product.id} className="product-card">
+            <h3>{product.name}</h3>
+            <p>₹{product.price}</p>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
